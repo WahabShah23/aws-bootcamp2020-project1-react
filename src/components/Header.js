e20d4d5ff448ec3b39e2e1a9332adc55d335ac5a
Wahab Shah <Wahab.shah23@gmail.com>
2020-06-22 13:41:35 +0500

    header updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,7 @@ import React from 'react'
 export const Header = () => {
     return (
         <div>
-            <h2>
-                Expense Tracker
-            </h2>
+            <h2>Expense Tracker</h2>
             <label> the <s>real</s> asli expense tracker </label>
         </div>
 
